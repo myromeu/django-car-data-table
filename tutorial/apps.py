80fc29b4333c133f1cbdf457e85f81a9ef0cab19
Ivan <myromeu@ya.ru>
2019-11-19 10:27:44 +0300

    add model and create objects on runserver

--- tutorial/apps.py
+++ tutorial/apps.py
@@ -1,5 +1,23 @@
+import sys
+import os
 from django.apps import AppConfig
 
 
 class TutorialConfig(AppConfig):
     name = 'tutorial'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return True
+
+        from .models import CarEvent
+        from django.conf import settings
+        import json
+
+        with open(os.path.join(settings.BASE_DIR, 'table-data.json')) as table_data:
+            json_data = json.loads(table_data.read())
+
+            for car in json_data:
+                # make CarEvent objects
+                # on stop server truncate the objects mb not
+                car_event = CarEvent.create(**car)
--- tutorial/models.py
+++ tutorial/models.py
@@ -3,3 +3,28 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100, verbose_name="full name")
+
+
+class CarEvent(models.Model):
+    ordNumber = models.IntegerField(primary_key=True)  # 1,
+    carNumber = models.CharField(max_length=16)  # "94003894",
+    trainIndex = models.CharField(max_length=16, null=True, blank=True) # "035004766035601",
+    trainNumber = models.CharField(max_length=16, null=True, blank=True)    # "9999",
+    carStatus = models.CharField(max_length=64, blank=True)  # "составлен в поезд",
+    invoiceId = models.CharField(max_length=16)  # "923210370",
+    invoiceNumber = models.CharField(max_length=16)  # "ЭО560089",
+    stateId = models.IntegerField()    # 34,
+    lastOperDt = models.DateTimeField(null=True, blank=True) # "2019-08-11T20:51:00.000Z"
+
+    @staticmethod
+    def create(**kwargs):
+        movie = .objects.create(
+            popularity=kwargs['99popularity'],
+            director=kwargs['director'],
+            imdb_score=kwargs['imdb_score'],
+            name=kwargs['name']
+        )
+        for genre_name in kwargs['genre']:
+            genre, created = Genre.objects.get_or_create(name=genre_name)
+            movie.genre.add(genre)
+        return movie
